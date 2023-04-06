2086205eec8d71979044509fac65d5ff3de8c592
Stevens <shanes@belpark.sun.ac.za>
2023-04-06 09:43:14 +0200

    update

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Ecommerce Plugin
+ * 
+ * @package Ecommerce Plugin
+ * @author Shane Stevens
+ * @copyright 2023 Shane Stevens
+ * 
+ * @wordpress-plugin
+ * Plugin Name: Ecommerce Plugin
+ * Description: Hi there If your reading this you probably installed my wordpress Plugin/Theme for an Ecommerce Website. This plugin creates a fully functional ecommerce website.(* Under construction *)
+ * Version: 0.1
+ * Author: Shane Stevens
+ * License: Free
+ */
+
+// ________________
+// This File acts as a form communication between all files which connects them along with all their functions 
+
+// ________________
+// call Files & Functions
+
+// ________________
+// Create Page Function
+
+// ________________
+// ONACTIVATING PLUGIN FUNCTION
+
+// ________________
+// ONDEACTIVATING PLUGIN FUNCTION
+
+?>
