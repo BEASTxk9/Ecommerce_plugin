9cd12c942a355508a8d623b375397a2714a1535c
Stevens <shanes@belpark.sun.ac.za>
2023-04-06 10:00:27 +0200

    update

--- main.php
+++ main.php
@@ -22,11 +22,67 @@
 
 // ________________
 // Create Page Function
+function create_page($title_of_the_page, $content, $parent_id = NULL)
+{
+	$objPage = get_page_by_title($title_of_the_page, 'OBJECT', 'page');
+	if (!empty($objPage)) {
+		// echo "Page already exists:" . $title_of_the_page . "<br/>";
+		return $objPage->ID;
+	}
+
+	$page_id = wp_insert_post(
+		array(
+			'comment_status' => 'close',
+			'ping_status' => 'close',
+			'post_author' => 1,
+			'post_title' => ucwords($title_of_the_page),
+			'post_name' => strtolower(str_replace(' ', '-', trim($title_of_the_page))),
+			'post_status' => 'publish',
+			'post_content' => $content,
+			'post_type' => 'page',
+			'post_parent' => $parent_id //'id_of_the_parent_page_if_it_available'
+		)
+	);
+	echo "Created page_id=" . $page_id . " for page '" . $title_of_the_page . "'<br/>";
+	return $page_id;
+}
+
 
 // ________________
-// ONACTIVATING PLUGIN FUNCTION
+// Create Table Function
+function create_table_on_activate()
+{
+	// connect to wordpress database
+	global $wpdb;
+
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+	$result = dbDelta($sql);
+
+
+	if (is_wp_error($result)) {
+		echo 'There was an error creating the tables';
+		return;
+	}
+
+}
+register_activation_hook(__FILE__, 'create_table_on_activate');
+
 
 // ________________
-// ONDEACTIVATING PLUGIN FUNCTION
+// ONACTIVATING PLUGIN FUNCTION(Create pages along with content from short code)
+function on_activating_your_plugin()
+{
 
+}
+register_activation_hook(__FILE__, 'on_activating_your_plugin');
+
+
+// ________________
+// ONDEACTIVATING PLUGIN FUNCTION(delete pages creates from onactivating the plugin)
+function on_deactivating_your_plugin()
+{
+
+}
+register_deactivation_hook(__FILE__, 'on_deactivating_your_plugin');
 ?>
